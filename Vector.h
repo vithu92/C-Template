7a63b24902ead7216b5c8ffaaa03e58ba468b67d
Vithushan Mahendran <vithushan.mahendran@hotmail.com>
2018-11-30 22:47:52 +0100

    last lesson

--- Vector.h
+++ Vector.h
@@ -15,27 +15,67 @@ public:
 	Vector(size_t capacity = 10)
 		: m_array(new T[capacity]), m_capacity(capacity), m_size(0) {};
 
+	Vector(const Vector& v)
+		: m_array(new T[v.m_capacity]), m_capacity(v.m_capacity), m_size(v.m_size) {
+		for (size_t i = 0; i < m_size; i++) {
+			m_array[i] = v[i];
+		}
+	}
+	Vector(Vector&& v) : m_array(move(v.m_array)), m_capacity(v.m_capacity), m_size(v.m_size) {
+		v.m_size = v.m_capacity = 0;
+	}
+
 	bool isEmpty() const { return m_size = 0; };
 
 	Vector(size_t size, const T& val)
-		: m_array(new T[size]), m_capacity(size), m_size(size){
+		: m_array(new T[size]), m_capacity(size), m_size(size) {
 		for (size_t i = 0; i < m_size; i++) m_array[i] = val;
 	}
 
+	Vector& operator=(const Vector& v) {
+		if (this != &v) {
+			m_array.reset(new T[v.m_capacity]);
+			m_capacity = v.m_capacity;
+			m_size = v.m_size;
+			for (size_t i = 0; i < m_size; i++) m_array[i] = v[i];
+		}
+		return *this;
+	}
+
+	Vector& operator=(Vector&& v) {
+		if (this != &v) {
+			m_array.release();
+			m_array.swap(v.m_array);
+			m_capacity = v.m_capacity;
+			m_size = v.m_size;
+			v.m_capacity = v.m_size = 0;
+		}
+		return *this;
+	}
+
 	//index Operator mit nur Lesezugriff - T als reference im Rückgabetyp da man nicht weiss was das für ein Typ sein wird. 
-	 const T& operator[](size_t i) const {
+	const T& operator[](size_t i) const {
 		if (i >= m_size) throw out_of_range("index is too large");
 		return m_array[i];
 	}
 
-	 T& operator[](size_t i) {
-		 if (i >= m_size) throw out_of_range("index is too large");
-		 return m_array[i];
-	 }
+	T& operator[](size_t i) {
+		if (i >= m_size) throw out_of_range("index is too large");
+		return m_array[i];
+	}
 
+	
+	
 	 void push_back(const T& data) {
 		 if (m_size == m_capacity) increaseCapacity();
 		 m_array[m_size++] = data;
+	 }
+
+	 // verwendung von Rechtsreferenten. 
+	 template<typename... Args> 
+	 T& emplace_back(Args&&... args) {
+		 if (m_size >= m_capacity) increaseCapacity();
+		 return m_array[m_size++] = T(forward<Args>(args)...);
 	 }
 
 	friend ostream& operator<<(ostream& os, const Vector& v) {
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "Vector.h"
+#include <vector>
+#include <set>
 
 class C {
 	int m_x;
@@ -9,24 +11,79 @@ public:
 	}
 };
 
+template<class T>
+class D {
+	T m_x;
+
+public:
+	D( T x = 0): m_x(x){}
+	friend ostream& operator<<(ostream& os, const D& d) {
+		return os << d.m_x;
+	}
+};
+
+/*template<template<typename, typename> class Con, class T, class A>
+ostream& operator<<(ostream& os, const Con<T,A>& c) {
+	os << "generisch[";
+	auto& it = c.begin();
+	if (it != c.end()) os << *it++;
+	while(it != c.end()) {
+		os << ',' << *it++;
+	}	
+	return os << ']';
+}*/
+
+template<template<typename...> class Con, class... Rest>
+ostream& operator<<(ostream& os, const Con<Rest...>& c) {
+	os << "generisch[";
+	auto it = c.begin();
+	if (it != c.end()) os << *it++;
+	while (it != c.end()) {
+		os << ',' << *it++;
+	}
+	return os << ']';
+}
 int main() {
 	Vector<double> v1(5, 1.9);
 
 	cout << v1 << endl;
 	v1[0] = 33;
 	cout << v1 << endl;
+	v1[2] = 33;
+	cout << v1 << endl;
+	Vector<double> v11;
+	v11 = v1;
+	cout << "nach Zuweisung: " << v11 << endl;
+	Vector<double> v12;
+	v12 = move(v11);
+	cout << "nach Verschiebung: " << v12 << endl;
+	cout << "verschobener Vektor: " << v11 << endl;
 
+	auto v13(v1);
+	cout << "nach copy-ctor: " << v13 << endl;
+
+	auto v14(move(13));
+	cout << "nach move-ctor: " << v14 << endl;
+	cout << "verschobener Vektor: " << v13 << endl;
 
 	Vector<C> v2(4, 100);
 	cout << v2 << endl;
 	v2.push_back(200);
 	v2.push_back(C(300));
 	cout << v2 << endl;
+
+
 	//Vector<C> v2(4, 100);
 	//cout << v2 << endl;
 
-	//Vector<Vector<double>> vv(3, v1);
-	//cout << vv << endl;
+	Vector<Vector<double>> vv(3, v1);
+	cout << vv << endl;
+	vv.emplace_back(2, 99.99);
+	cout << vv << endl;
 
+	cout << D<int>(4) << endl;
+
+	set<double> s1 = { 1.1, 2.2, 3.3 };
+	cout << s1 << endl;
 
 }
